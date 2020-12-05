25c84ace4674e37580252a1e4590c528da33f92a
Лев Бондаренко <lion.bond93@gmail.com>
2020-12-05 17:02:32 +0500

    init

---
+++ Pods/Target Support Files/Pods-swift-figma-export/Pods-swift-figma-export-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_swift_figma_export : NSObject
+@end
+@implementation PodsDummy_Pods_swift_figma_export
+@end
---
+++ swift-figma-export/AppLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-figma-export
+//
+//  Created by Лев Бондаренко on 05.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
